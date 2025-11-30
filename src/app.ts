e2d33f47d910eeb4c07a8df5896e38243c772974
MAKSIM <max_air@bk.ru>
2025-11-30 21:20:20 +0300

    feat: add car field

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import { navigation } from "./components/navigation";
+import { carField } from "./components/carField";
+const garage = document.querySelector(".car_wrapper");
 
 export function app() {
   navigation();
+  garage!.innerHTML += carField({ name: "Mitsubishi", color: "#fede00", id: 2 });
+  // garage?.appendChild(carField({ name: "Mitsubishi", color: "#fede00", id: 2 }));
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,34 +1,7 @@
-export interface IImgSource {
-  src: string;
+export interface ICar {
   name: string;
-  author: string;
-  year: string;
-  pages: number;
-}
-
-export interface IWordSource {
-  audioExample: string;
-  textExample: string;
-  textExampleTranslate: string;
+  color: string;
   id: number;
-  word: string;
-  wordTranslate: string;
+  // velocity: number;
+  // distance: number;
 }
-
-export enum Position {
-  TRUE = "0px -5px 6px -5px rgba(122, 208, 20, 1) inset",
-  FALSE = "0px -5px 6px -5px rgba(255, 0, 0, 1) inset",
-  RESET = "none",
-}
-
-export interface IStatistics {
-  IDontKnow: IStatObject[];
-  IKnow: IStatObject[];
-}
-
-interface IStatObject {
-  word: string;
-  src: string;
-}
-
-export type TPaintData = Omit<IImgSource, "pages">;
